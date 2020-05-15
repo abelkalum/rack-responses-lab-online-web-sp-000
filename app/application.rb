656418484a6174dd9b8dc247d0bbcc3820b8ee78
Abel Kalum <chumbabel@gmail.com>
2020-05-15 15:10:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,16 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
- 
-    if Time.now.to_i ? < 12:00 PM
+
+    if Time.now.hour.between?(0, 11)
       resp.write "Good Morning!"
+    elsif Time.now.hour.between?(12, 17)
+      resp.write "Good Afternoon."
     else
-      resp.write "Good Afternoon!"
+      resp.write "Good Evening!"
     end
- 
+
     resp.finish
   end
- 
 end
